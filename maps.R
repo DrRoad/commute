3d5b96f0651a89d0796cbd3a6fb3a2c52cac7978
Petra Lamborn <petra@petras.space>
2020-06-25 11:59:11 +1200

    Simplify shapefiles

--- maps.R
+++ maps.R
@@ -2,20 +2,29 @@ library(rgdal)
 library(rmapshaper)
 
 sf <- readOGR(dsn = "shapefiles/statsnzstatistical-area-2-2018-generalised-SHP/")
+nrow(sf@data)
 
-plot(sf, xlim=c(1750000, 1760000), ylim=c(5430000, 5440000))
+plot(sf, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
 # insf <- ms_innerlines(sf)
 
-ssf1 <- ms_simplify(sf)
-plot(ssf1, xlim=c(1750000, 1760000), ylim=c(5430000, 5440000))
+ssf1 <- ms_simplify(sf, keep_shapes=TRUE)
+plot(ssf1, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
+nrow(ssf1@data)
 
-ssf2.5 <- ms_simplify(sf, 0.025)
-plot(ssf2.5, xlim=c(1750000, 1760000), ylim=c(5430000, 5440000))
+ssf2.5 <- ms_simplify(sf, 0.025, keep_shapes=TRUE)
+plot(ssf2.5, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
+nrow(ssf2.5@data)
 
-ssf2 <- ms_simplify(sf, 0.01)
-plot(ssf2, xlim=c(1750000, 1760000), ylim=c(5430000, 5440000))
+ssf2 <- ms_simplify(sf, 0.01, keep_shapes=TRUE)
+plot(ssf2, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
+nrow(ssf2@data)
 
-ssf3 <- ms_simplify(sf, 0.002)
-plot(ssf3, xlim=c(1750000, 1760000), ylim=c(5430000, 5440000))
+ssf3 <- ms_simplify(sf, 0.002, keep_shapes=TRUE)
+plot(ssf3, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
+nrow(ssf3@data)
 
-writeOGR(ssf2, dsn = "shapefiles/sa2001/", layer = "SA2", driver="ESRI Shapefile")
+writeOGR(ssf2.5, dsn = "shapefiles/sa20025/", layer = "SA2", driver="ESRI Shapefile")
+
+ssf3b <- ms_simplify(sf, 0.002, keep_shapes=FALSE)
+plot(ssf3b, xlim=c(1480000, 1510000), ylim=c(5150000, 5180000))
+nrow(ssf3b@data)
