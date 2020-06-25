a204b2821a010bfebbb82f5bf5b30af37b913188
Petra Lamborn <oleumpetra@gmail.com>
2020-06-25 20:41:06 +1200

    make actual shiny, reshaping data

--- scratch.R
+++ scratch.R
@@ -27,3 +27,68 @@ locgraph <- graph(edges = elist)
 # plot(locgraph, label = NA)
 
 sg <- decompose(locgraph, mode="weak")
+
+
+work_travel %>% rename(
+  res_code = SA2_code_usual_residence_address,
+  res_name = SA2_name_usual_residence_address,
+  res_east = SA2_usual_residence_easting,
+  res_north = SA2_usual_residence_northing,
+  work_code = SA2_code_workplace_address,
+  work_name = SA2_name_workplace_address,
+  work_east = SA2_workplace_easting,
+  work_north = SA2_workplace_northing,
+  home = Work_at_home,
+  private = Drive_a_private_car_truck_or_van,
+  company = Drive_a_company_car_truck_or_van,
+  passenger = Passenger_in_a_car_truck_van_or_company_bus,
+  bus = Public_bus,
+  train = Train,
+  bicycle = Bicycle,
+  walk = Walk_or_jog,
+  ferry = Ferry,
+  other = Other,
+  total = Total
+) -> work_simp
+
+work_simp %>% 
+  group_by(res_code,
+                         res_name,
+                         res_east,
+                         res_north) %>%
+  summarise(
+    home = sum(ifelse(home < 0, 0, home)),
+    private = sum(ifelse(private < 0, 0, private)),
+    company = sum(ifelse(company < 0, 0, company)),
+    passenger = sum(ifelse(passenger < 0, 0, passenger)),
+    bus = sum(ifelse(bus < 0, 0, bus)),
+    train = sum(ifelse(train < 0, 0, train)),
+    bicycle = sum(ifelse(bicycle < 0, 0, bicycle)),
+    walk = sum(ifelse(walk < 0, 0, walk)),
+    ferry = sum(ifelse(ferry < 0, 0, ferry)),
+    other = sum(ifelse(other < 0, 0, other)),
+    total = sum(ifelse(total < 0, 0, total)), .groups="drop"
+  ) -> work_from
+    
+
+work_simp %>% 
+  group_by(work_code,
+                         work_name,
+                         work_east,
+                         work_north) %>%
+  summarise(
+    home = sum(ifelse(home < 0, 0, home)),
+    private = sum(ifelse(private < 0, 0, private)),
+    company = sum(ifelse(company < 0, 0, company)),
+    passenger = sum(ifelse(passenger < 0, 0, passenger)),
+    bus = sum(ifelse(bus < 0, 0, bus)),
+    train = sum(ifelse(train < 0, 0, train)),
+    bicycle = sum(ifelse(bicycle < 0, 0, bicycle)),
+    walk = sum(ifelse(walk < 0, 0, walk)),
+    ferry = sum(ifelse(ferry < 0, 0, ferry)),
+    other = sum(ifelse(other < 0, 0, other)),
+    total = sum(ifelse(total < 0, 0, total)), .groups="drop"
+  ) -> work_to
+
+tencols <-  c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", 
+              "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a")
--- viz/app.R
+++ viz/app.R
@@ -1,19 +1,39 @@
 library(shiny)
 library(leaflet)
 library(rgdal)
+library(readr)
+library(dplyr)
 
-sf <- readOGR(dsn="../shapefiles/sa20025WGSfilcth")
-leaflet(sf, options = leafletOptions(minZoom = 3, maxZoom = 13)) %>% 
-  addPolygons(color="#000", opacity = 1, weight=1,
-                            popup = sf@data$SA22018__1) %>%
-  setView(174, -41, 5)
+work_travel <- read_csv("../travel-work.csv")
+shpf <- readOGR(dsn="../shapefiles/sa20025WGSfilcth")
 
 # Define UI
 ui <- fluidPage(
+  leafletOutput("map")
 )
 
 # Define server logic
 server <- function(input, output) {
+  output$map <- renderLeaflet({
+    leaflet(shpf, options = leafletOptions(minZoom = 3, maxZoom = 13)) %>% 
+      addPolygons(color="#000", opacity = 1, weight=1,
+                                popup = shpf@data$SA22018__1) %>%
+      setView(174, -41, 5)
+  })
+  observeEvent(input$map_shape_click, {
+    p <- input$map_shape_click
+    print(p)
+    pdat <- data.frame(Longitude = p$lng,
+                      Latitude =p$lat)
+    # Assignment modified according
+    coordinates(pdat) <- ~ Longitude + Latitude
+    # Set the projection of the SpatialPointsDataFrame using the projection of the shapefile
+    proj4string(pdat) <- proj4string(shpf)
+    ppoly <- over(pdat, shpf)
+    print(ppoly)
+    print(work_travel[work_travel$SA2_code_usual_residence_address == 
+                        ppoly[1,"SA22018_V1"],])
+  })
 }
 
 # Run the application 
